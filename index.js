e499e9fe8a8689b6388f5dd612bae963078dfa8b
pratik-bongale <psb4346@rit.edu>
2020-05-17 01:39:17 -0400

    add button for refresh

---
+++ index.js
@@ -0,0 +1,30 @@
+// Set an image on dice 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
+
+var dice1 = document.querySelector(".dice1");
+
+dice1.setAttribute("src", randomImageSource1);
+
+
+// Set an image on dice 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+var dice2 = document.querySelector(".dice2");
+
+dice2.setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
